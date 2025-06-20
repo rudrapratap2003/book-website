513cd5ea32afb70bda6bbd5c122ea042bbef3f1a
rudrapratap2003 <126893427+rudrapratap2003@users.noreply.github.com>
2025-06-20 11:41:20 +0530

    Delete backend directory

--- backend/src/routes/book.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sellBook, buyBook } from "../controllers/book.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router = Router()
-
-router.route("/sell-book").post(verifyJWT, sellBook) // http://localhost:8000/api/v1/books/sell-book   
-router.route('/:bookId').post(verifyJWT, buyBook)
-export default router
--- backend/src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse {
-    constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400 // i.e it will true if status code < 400 else it will show false (i.e if error code is genertaed)
-    }
-}
-export {ApiResponse}
