64dd3300a3580b4a2a7ca9a5103fece6525a9711
iammonali <monalisasamal1653@gmail.com>
2025-06-23 13:07:38 +0530

    My Profile page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Buy from "./components/Buy";
 import SignUp from "./components/SignUp";
 import Signin from "./components/Signin";
 import RefreshHandler from "./components/RefreshHandler";
+import MyProfile from "./components/MyProfile";
 const books = [
   { 
     id: 1, 
@@ -94,6 +95,7 @@ function App() {
         <Route path="/sell" element={<PrivateRoute element={<Sell />}/>}/>
         <Route path="/buy" element={<PrivateRoute element={<Buy />}/>}/>
         <Route path="/exchange" element={<PrivateRoute element={<Exchange />}/>}/>
+        <Route path="/myprofile" element={<PrivateRoute element={<MyProfile />}/>}/>
       </Routes>
       <Footer />
     </Router>
