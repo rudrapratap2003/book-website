10c56fc3cf6409efa3e47982acddad151469985b
Rudra Pratap Padhi <rudrapratappadhi2003@gmail.com>
2025-07-08 22:37:19 +0530

    deployment checked

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api/v1': 'http://localhost:5000'
+      '/api/v1': 'https://book-website-f9e4.onrender.com'
     }
   },
   plugins: [react()],
